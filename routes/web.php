ad865dc35c002479cb904a3ebe92035fc7329c7b
AldiAgeng <aldiageng48@gmail.com>
2021-10-06 20:06:46 +0700

    Selesai latihan video Belajar Laravel 8 | 3. Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Aldi Ageng',
+        'email' => 'aldiageng...@gmail.com',
+        'image' => 'gambar.png'
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
