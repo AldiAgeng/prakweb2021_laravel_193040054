c3b4181d079c981ab74e2ef40689463580a7f683
AldiAgeng <aldiageng48@gmail.com>
2021-10-14 08:34:43 +0700

    Selesai latihan video Belajar Laravel 8 | 7. Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,37 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            'title' => 'Judul Post Pertama',
-            'slug' => 'judul-post-pertama',
-            'author' => 'Aldi Ageng',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima nisi iste corporis suscipit architecto quod, inventore hic voluptates quasi numquam laborum dolorum, debitis, minus sed nostrum. Saepe, ipsum unde autem quisquam nesciunt maxime eum ullam fuga facere, accusamus vel quia quaerat repellat vitae id quidem velit nostrum placeat assumenda, explicabo laboriosam voluptatum. Nemo pariatur accusamus, amet nesciunt commodi tempore facere.'
-        ],
-        [
-            'title' => 'Judul Post Kedua',
-            'slug' => 'judul-post-kedua',
-            'author' => 'Ujang',
-            'body' => ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt molestiae magnam saepe hic numquam. Ab sit dolorum voluptatibus ipsam, doloribus eos eius veniam tempore rem aspernatur officiis saepe quasi. Quae itaque asperiores explicabo adipisci error, laudantium saepe veritatis alias recusandae corporis at illum nemo rerum, velit excepturi laborum neque eius odit ipsum commodi doloremque? Sit saepe vitae quos reprehenderit officiis error labore, unde cum iusto ratione vero aperiam. In itaque nemo atque ut. Optio quaerat quod beatae dolorem quibusdam corrupti!'
-        ],
-    ];
+    use HasFactory;
 
-    public static function all(){
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug){
-        $posts = static::all();
-
-        // $post = [];
-        // foreach ($posts as $p) {
-        //     if($p['slug'] == $slug){
-        //         $post = $p;
-        //     }
-        // }
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
