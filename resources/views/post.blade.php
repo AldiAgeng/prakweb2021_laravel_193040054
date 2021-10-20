6f2a3226eea8bf6a1b0614d0598e4f56f90cbecf
AldiAgeng <aldiageng48@gmail.com>
2021-10-20 19:46:33 +0700

    selesai video Belajar Laravel 8 | 9. Database Seeder

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,8 @@
 
 @section('container')
     <h2>{{ $post->title }}</h2>
-    <p>By. Aldi Ageng in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a class="text-decoration-none" href="#">{{ $post->user->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 
-  <a href="/posts">Back to Posts</a>
+  <a href="/posts" class="text-decoration-none d-block mt-3">Back to Posts</a>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,11 +3,14 @@
 <h1 class="mb-4">Halaman Posts</h1>
 
 @foreach ($posts as $post)
-<article class="mb-5">
+<article class="mb-5 border-bottom pb-4">
   <h2>
-  <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+  <a class="text-decoration-none" href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+  <p>By. <a class="text-decoration-none href="#">{{ $post->user->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
   </h2>
   <p>{{ $post->excerpt }}</p>
+
+  <a class="text-decoration-none" href="/posts/{{ $post->slug }}">Read more..</a>
 </article>  
 @endforeach
   
